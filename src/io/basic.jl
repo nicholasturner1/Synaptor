81c983def118ea567f065cb20771eda9cca09f38
nicholasturner1 <nturner.stanford@gmail.com>
2017-04-13 22:52:08 -0400

    Adding some IO for semmaps

--- src/io/basic.jl
+++ src/io/basic.jl
@@ -2,6 +2,7 @@ module BasicIO
 
 
 export read_edge_file
+export read_semmap, write_semmap
 
 
 function read_csv(fname, T::DataType)
@@ -32,6 +33,89 @@ function read_edge_file(fname)
 
   [(arr[i,1],arr[i,2]) for i in 1:size(arr,1)]
 end
+
+
+"""
+
+    read_map_file( input_fname; delim=';' )
+
+  Reads in map files as written by write_map_file.
+"""
+function read_map_file( input_fname; delim=';' )
+
+  num_fields = 0 #init
+  Ts = DataType[];
+
+  open(input_fname) do f
+    first_ln = readline(f)
+    fields = map(x -> eval(parse(x)), split(first_ln,"$delim"))
+    num_fields = length(fields)-1 #-1 for key
+    Ts = map(typeof, fields)
+  end
+
+  dicts = [Dict{Ts[1],Ts[i+1]}() for i=1:num_fields];
+
+  open(input_fname) do f
+
+    for ln in eachline(f)
+
+      fields = map(x -> eval(parse(x)), split(ln,"$delim"));
+      key = fields[1]
+
+      for i in 1:length(dicts)  dicts[i][key] = fields[i+1]  end
+
+    end #for ln
+
+  end#open(input_fname)
+
+  dicts
+end
+
+
+"""
+
+    write_map_file( output_fname, dicts...; delim=';' )
+
+  Takes a collection of dictionaries which are assumed to have the
+  same keys (or, at least those of the first dict). Writes a file
+  in which each line takes the form
+
+  key;val1;val2... for the number of dictionaries
+"""
+function write_map_file( output_fname, dicts...; delim=';' )
+
+  open(output_fname, "w+") do f
+    if length(dicts) > 0
+    for k in keys(dicts[1])
+
+      valstring = join(["$(d[k])" for d in dicts ], delim);
+      write(f, "$k;$valstring\n" )
+
+    end #for k
+    end #if length
+  end #open(fname)
+
+end
+
+
+"""
+
+    read_semmap(input_fname)
+
+  Reads a semicolon-delimited csv file as a pair of Dicts representing
+  the result of a semantic mapping
+"""
+read_semmap(input_fname) = read_map_file(input_fname)
+
+
+"""
+
+    write_semmmap(assignment, [weights], output_fname)
+
+  Saves a semantic id map as a semicolon-delimited csv file
+"""
+write_semmap(a,w,output_fname) = write_map_file(output_fname, a,w)
+write_semmap(a,output_fname) = write_map_file(output_fname, a)
 
 
 end #module BasicIO
--- src/io/include.jl
+++ src/io/include.jl
@@ -2,6 +2,7 @@ module IO
 
 #BasicIO
 export read_edge_file
+export read_semmap, write_semmap
 
 include("basic.jl"); using .BasicIO
 
