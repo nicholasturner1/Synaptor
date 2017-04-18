4f9d1dbf2069e952adb5fe4454e926f7c25801c7
nicholasturner1 <nturner.stanford@gmail.com>
2017-04-18 11:35:50 -0400

    Plugging in EF interface changes

--- src/Synaptor.jl
+++ src/Synaptor.jl
@@ -16,14 +16,15 @@ export neighborhood_semmaps
 export PrePostEdgeFinder, findedges_w_prepost
 export VesicleEdgefinder, findedges_w_ves
 export SemanticEdgeFinder, findedges_w_sem
-export assign_aux_params!, assign_aux_vols!, assign_ccs!
-export findedges
+export assign_aux_params!, assign_aux_vols!
+export make_ccs!, get_ccs, compute_cc_stats
+export filter_by_size!, filter_by_id!, dilate_ccs!
+export find_continuations
 #Eval
 export prec_score, rec_score, f1score, f0p5score
 #Proc
 export process_single_chunk
 export grid_search, f1_grid_search, f0p5_grid_search
-#CloudUtils
 
 
 include("Types.jl")
@@ -40,7 +41,11 @@ include("segutils/include.jl"); using .SegUtils
 include("edgefinders/include.jl"); using .EdgeFinders
 include("eval/include.jl"); using .Eval
 include("proc/include.jl"); using .Proc
-include("cloudutils/include.jl");
+
+
+##OPTIONAL CLOUD UTILITIES
+##export sendmsg, pullmsg, delmsg
+##include("cloudutils/include.jl");
 
 
 end # module Synaptor
