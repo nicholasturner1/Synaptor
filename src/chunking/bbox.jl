cc3355cf5e4c9694c8d1eed9e96dfc4e3957a77f
nicholasturner1 <nturner.stanford@gmail.com>
2017-04-25 12:12:11 -0400

    Adding specific indexing fns for bboxes

--- src/chunking/bbox.jl
+++ src/chunking/bbox.jl
@@ -4,6 +4,7 @@ module BBoxes
 export BBox
 
 using HDF5
+using BigArrays
 
 
 immutable Vec3
@@ -44,7 +45,8 @@ immutable BBox
 
   BBox(f::Vec3,l::Vec3) = new(f,l)
   BBox(x1,y1,z1,x2,y2,z2) = new(Vec3(x1,y1,z1),Vec3(x2,y2,z2))
-  BBox(v1,v2) = new(Vec3(v1[1],v1[2],v1[3]),Vec3(v2[1],v2[2],v2[3]))
+  BBox(v1::Vector,v2::Vector) = new(Vec3(v1[1],v1[2],v1[3]),Vec3(v2[1],v2[2],v2[3]))
+  BBox(v1::Tuple,v2::Tuple) = new(Vec3(v1[1],v1[2],v1[3]),Vec3(v2[1],v2[2],v2[3]))
 
   function BBox(p::Pair)
     x1,y1,z1 = p.first
@@ -68,8 +70,12 @@ Base.:(-)(bbox::BBox, v) = bbox - Vec3(v)
 Base.:(&)(b1::BBox, b2::BBox) = BBox(max(b1.f,b2.f),min(b1.l,b2.l))
 
 Base.show(io::IO, bbox::BBox) = print(io, "BBox{$(bbox.f)<->$(bbox.l)}")
-Base.size(bbox::BBox) = bbox.l - bbox.f + 1
+Base.size(bbox::BBox) = collect(bbox.l - bbox.f + 1)
 
+
+zipindices(bbox::BBox) = [bbox.f.x:bbox.l.x,
+                          bbox.f.y:bbox.l.y,
+                          bbox.f.z:bbox.l.z]
 
 @inline function Base.getindex{T}(a::AbstractArray{T,3}, bbox::BBox)
   a[bbox.f.x:bbox.l.x,
@@ -79,7 +85,7 @@ end
 
 
 function Base.getindex{T,N}(a::AbstractArray{T,N}, bbox::BBox)
-  
+
   indices = Vector{Any}(N)
   indices[:] = Colon()
 
@@ -142,7 +148,7 @@ end
 
 #ditto for h5datasets
 function Base.getindex(a::HDF5Dataset, bbox::BBox)
-  
+
   indices = Vector{Any}(length(size(a)))
   indices[:] = Colon()
 
@@ -183,6 +189,64 @@ end
 
 
 function Base.setindex!(a::HDF5Dataset, X, bbox::BBox, I...)
+
+  indices = Vector{Any}(length(size(a)))
+  indices[:] = Colon()
+
+  indices[1] = bbox.f.x:bbox.l.x
+  indices[2] = bbox.f.y:bbox.l.y
+  indices[3] = bbox.f.z:bbox.l.z
+
+  for (i,v) in enumerate(I)  indices[3+i] = v  end
+
+  a[indices...] = X
+end
+
+#ditto for BigArrays
+#unfortunately, I need to write these so they're the most specific
+# or at least, I don't know a way around this
+function Base.getindex(a::BigArray, bbox::BBox)
+
+  indices = Vector{Any}(length(size(a)))
+  indices[:] = Colon()
+
+  indices[1] = bbox.f.x:bbox.l.x
+  indices[2] = bbox.f.y:bbox.l.y
+  indices[3] = bbox.f.z:bbox.l.z
+
+  a[indices...]
+end
+
+
+function Base.getindex(a::BigArray, bbox::BBox, I...)
+
+  indices = Vector{Any}(length(size(a)))
+  indices[:] = Colon()
+
+  indices[1] = bbox.f.x:bbox.l.x
+  indices[2] = bbox.f.y:bbox.l.y
+  indices[3] = bbox.f.z:bbox.l.z
+
+  for (i,v) in enumerate(I)  indices[3+i] = v  end
+
+  a[indices...]
+end
+
+
+function Base.setindex!(a::BigArray, X, bbox::BBox)
+
+  indices = Vector{Any}(length(size(a)))
+  indices[:] = Colon()
+
+  indices[1] = bbox.f.x:bbox.l.x
+  indices[2] = bbox.f.y:bbox.l.y
+  indices[3] = bbox.f.z:bbox.l.z
+
+  a[indices...] = X
+end
+
+
+function Base.setindex!(a::BigArray, X, bbox::BBox, I...)
 
   indices = Vector{Any}(length(size(a)))
   indices[:] = Colon()
