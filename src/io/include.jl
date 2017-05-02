e8b761bfe6feaee79ebd85e5ace725634fc5ee3c
nicholasturner1 <nturner.stanford@gmail.com>
2017-05-02 14:49:47 -0400

    Feather-based IO

--- src/io/include.jl
+++ src/io/include.jl
@@ -1,17 +1,18 @@
 module InputOutput
 
-#BasicIO
+#BasicIO/FeatherIO
 export read_edge_file, write_edge_file
 export read_semmap, write_semmap
 export read_idmap, write_idmap
-export read_focal_pts, write_focal_pts
+#export read_focal_pts, write_focal_pts
 #ConsolidationIO
 export write_continuation, write_continuations
 #SegIO
 export write_in_chunks, create_seg_dset
 
-include("basic.jl"); using .BasicIO
+#include("basic.jl"); using .BasicIO
 include("continuations.jl"); using .ContinuationIO
 include("segio.jl"); using .SegIO
+include("feather.jl"); using .FeatherIO
 
 end
