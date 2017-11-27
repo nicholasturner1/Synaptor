b01e88c7c93bc1c3eeb0c5613025a9764bcf7e5a
nicholasturner1 <nturner.stanford@gmail.com>
2017-11-26 23:56:15 -0500

    updating chunk_ccs a bit

--- tasks/chunk_ccs.py
+++ tasks/chunk_ccs.py
@@ -9,37 +9,33 @@ Chunkwise Connected Components
 -Write info for each segment (COM, size, bounding box) to the proc directory
 -Write continuations (segments contacting the chunk edges) to the proc directory
 """
-import Synaptor as S
+import synaptor as s
 
 
-def main(psd_cvname, cc_cvname, chunk_bounds, cc_thresh,
-         sz_thresh, dil_param, proc_dir_path):
+def main(psd_cvname,  cc_cvname, proc_dir_path,
+         chunk_begin, chunk_end, cc_thresh,
+         sz_thresh,   dil_param):
 
-    chunk_bounds = S.IO.parse_chunk_bounds(chunk_bounds)
+    chunk_bounds = s.BBox3d(chunk_begin, chunk_end)
 
     #Reading
-    psd_output = S.IO.read_cloud_volume_chunk(psd_cvname, chunk_bounds)
+    psd_output = s.io.read_cloud_volume_chunk(psd_cvname, chunk_bounds)
 
 
     #Processing
-    dil_ccs = S.dilated_conncomps(psd_output, cc_thresh, dil_param)
-    dil_ccs, sizes = S.filter_segs_by_size(dil_ccs, sz_thresh)
+    dil_ccs = s.dilated_components(psd_output, cc_thresh, dil_param)
+    dil_ccs, sizes = s.filter_segs_by_size(dil_ccs, sz_thresh)
 
-    offset  = S.extract_offset(chunk_bounds)
-    centers = S.centers_of_mass(dil_ccs, chunk_bounds)
-    bboxes  = S.bounding_boxes(dil_ccs, chunk_bounds)
+    offset  = chunk_bounds.min()
+    centers = s.centers_of_mass(dil_ccs, offset=offset)
+    bboxes  = s.bounding_boxes(dil_ccs, offset=offset)
 
-    continuations = S.find_continuations(dil_ccs)
-    sizes,   cont_sizes   = split_seg_cont_info(sizes, continuations)
-    bboxes,  cont_bboxes  = split_seg_cont_info(bboxes, continuations)
-    centers, cont_centers = split_seg_cont_info(centers, continuations)
-
+    continuations = s.extract_continuations(dil_ccs)
 
     #Writing
     S.IO.write_cloud_volume_chunk(dil_ccs, cc_cvname, chunk_bounds)
-    S.IO.write_chunk_continuations(continuations, cont_centers, cont_bboxes,
-                                   cont_sizes, proc_dir_path)
-    S.IO.write_chunk_seg_info(centers, sizes, bboxes, proc_dir_path)
+    S.IO.write_chunk_continuations(continuations, proc_dir_path)
+    S.IO.write_chunk_seg_info(centers, sizes, bboxes, chunk_bounds, proc_dir_path)
 
 
 def split_seg_cont_info(info_dict, continuations):
@@ -71,14 +67,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
 
+    # Input & Outputs
     parser.add_argument("psd_cvname")
     parser.add_argument("cc_cvname")
-    parser.add_argument("chunk_bounds", nargs="+", type=int)
+    parser.add_argument("proc_dir_path")
 
+    #Chunk Bounds
+    parser.add_argument("--chunk_begin", nargs="+", type=int, required=True)
+    parser.add_argument("--chunk_end", nargs="+", type=int, required=True)
+
+    # Processing Parameters
     parser.add_argument("cc_thresh")
     parser.add_argument("sz_thresh")
     parser.add_argument("dil_param")
-    parser.add_argument("proc_dir_path")
 
 
     args = parser.parse_args()
