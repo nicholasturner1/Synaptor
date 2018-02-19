ad476e515dc104bcb4f6615e33ec410a04dce9fc
nicholasturner1 <nturner.stanford@gmail.com>
2018-02-18 23:54:08 -0500

    Starting DataFrame type

---
+++ synaptor/types/dataframe/dataframe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+from enum import Enum
+
+import pandas as pd
+
+from . import dtypes
+
+
+class DataFrame(object):
+
+    def __init__(self, dicts, schema):
+        pass
+
+
+
