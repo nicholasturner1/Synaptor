eb2411a08a4831d310876e28c3df1dbdb851d3be
nicholasturner1 <nturner.stanford@gmail.com>
2018-03-18 17:55:27 -0400

    Updating dispatcher.sh

--- tasks/dispatcher.sh
+++ tasks/dispatcher.sh
@@ -3,7 +3,7 @@
 case $1 in
     chunk_ccs)   python3 chunk_ccs.py ${@:2} ;;
     merge_ccs)   python3 merge_ccs.py ${@:2} ;;
-    asynet_pass) python3 asynet_pass.py ${@:2} ;;
+    chunk_edges) python3 chunk_edges.py ${@:2} ;;
     merge_edges) python3 merge_edges.py ${@:2} ;;
     remap_ids)   python3 remap_ids.py ${@:2} ;;
     *)  echo "invalid task name $1"
