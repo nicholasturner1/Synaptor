610c87756bc8af4d80b6ae693d65e4f6f402ef43
nicholasturner1 <nturner.stanford@gmail.com>
2018-12-21 13:12:20 -0500

    Documentation for seg_utils module

--- synaptor/seg_utils/__init__.py
+++ synaptor/seg_utils/__init__.py
@@ -1,3 +1,27 @@
+__doc__ = """
+Segmentation Utility Functions
+
+A set of common functions for working with 3d segmentation volumes.
+
+These functions include:
+- Centroid coordinates (centers_of_mass)
+- Bounding boxes (bounding_boxes)
+- Segment sizes (segment_sizes)
+- General data relabeling (relabel_data)
+- Relabeling segment ids to 1:N (relabel_data_1N)
+- Finding nonzero unique ids (nonzero_unique_ids)
+- High-pass size thresholding (filter_segs_by_size)
+- General segment removal (filter_segs_by_id)
+- Computing overlap sizes (count_overlaps)
+- Splitting segments by overlap with a base (split_by_overlap)
+- Down/Upsampling over the last 2 dimensions (upsample2d, downsample_seg)
+- Dilation over the last 2 dimensions (dilate_by_k)
+
+Other functions exist. See the documentation for each function (and submodules)
+for more information.
+
+Nicholas Turner <nturner@cs.princeton.edu>, 2017-8
+"""
 from . import describe
 from .describe import *
 
