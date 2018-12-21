efa12a532279a3c0fd2dab198e1dee54acfcec80
nicholasturner1 <nturner.stanford@gmail.com>
2018-12-21 13:09:40 -0500

    Merge functions for anchor tasks

---
+++ synaptor/proc_tasks/anchor/merge.py
@@ -0,0 +1,15 @@
+def merge_anchors(anchor_dframes, edge_dframe):
+
+    df = pd.concat(anchor_dframes, copy=False)
+
+    df[df["presyn_x"] != -2]  # error code for no point
+
+    return df.groupby("cleft_segid.1").first()  # take random valid pt
+
+
+def merge_to_edge_df(anchor_dframe, edge_dframe):
+
+    df = pd.merge(anchor_dframe, edge_dframe, copy=False,
+                  left_index=True, right_index=True)
+
+    df.drop(["
