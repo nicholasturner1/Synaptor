d7000abd36a0c8cca75d2e9af3f5fdfa2730e215
nicholasturner1 <nturner.stanford@gmail.com>
2019-03-17 19:54:27 -0400

    adding new task to dispatcher script

--- tasks/dispatcher.sh
+++ tasks/dispatcher.sh
@@ -4,6 +4,7 @@ case $1 in
     chunk_ccs)        python3 chunk_ccs.py ${@:2} ;;
     match_contins)    python3 match_contins.py ${@:2} ;;
     seg_graph_ccs)    python3 seg_graph_ccs.py ${@:2} ;;
+    chunk_seg_map)    python3 chunk_seg_map.py ${@:2} ;;
     merge_seginfo)    python3 merge_seginfo.py ${@:2} ;;
     chunk_edges)      python3 chunk_edges.py ${@:2} ;;
     pick_edge)        python3 pick_edge.py ${@:2} ;;
