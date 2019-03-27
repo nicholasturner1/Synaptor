17ceb2ac257543140647957dfe22441f8606936a
nicholasturner1 <nturner.stanford@gmail.com>
2019-03-27 11:33:57 -0400

    bugfixes in create_index task script

--- tasks/create_index.py
+++ tasks/create_index.py
@@ -11,14 +11,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Inputs & Outputs
-parser.add_argument("url")
-parser.add_argument("tablename", type=int)
-parser.add_argument("colname", type=int)
+parser.add_argument("proc_url")
+parser.add_argument("tablename")
+parser.add_argument("colname")
 
 
 args = parser.parse_args()
-args.url = s.io.parse_proc_url(args.url)
+args.proc_url = s.io.parse_proc_url(args.proc_url)
 print(vars(args))
 
 
-s.io.create_index(**vars(args))
+s.io.create_index(args.proc_url, args.tablename, args.colname)
