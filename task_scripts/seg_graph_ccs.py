22d4f58bd0286082474987d1bb178d6158709859
nicholasturner1 <nturner.stanford@gmail.com>
2020-03-04 17:47:39 -0500

    fixing weird typos in script

--- task_scripts/seg_graph_ccs.py
+++ task_scripts/seg_graph_ccs.py
@@ -11,14 +11,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Inputs & Outputs
-parser.add_argument("stroagestr")
+parser.add_argument("storagestr")
 parser.add_argument("hashmax", type=int)
 
 parser.add_argument("--timing_tag", default=None)
 
 
 args = parser.parse_args()
-args.stroagestr = s.io.parse_stroagestr(args.stroagestr)
+args.storagestr = s.io.parse_storagestr(args.storagestr)
 print(vars(args))
 
 
