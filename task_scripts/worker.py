27e5bf22b8db622a5e1a74c51e523ef24bfc1008
nicholasturner1 <nturner.stanford@gmail.com>
2020-10-10 15:28:56 -0400

    TaskQueue figures out the queue type on its own now

--- task_scripts/worker.py
+++ task_scripts/worker.py
@@ -9,5 +9,5 @@ parser.add_argument("qurl", type=str)
 parser.add_argument("lease_seconds", type=int, default=300)
 
 args = parser.parse_args()
-with TaskQueue(qurl=args.qurl, queue_server="sqs", n_threads=0) as tq:
+with TaskQueue(qurl=args.qurl, n_threads=0) as tq:
     tq.poll(lease_seconds=args.lease_seconds)
